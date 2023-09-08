52b57fe488707e761e69a28ef4694bb10a1d13f4
Logan Marchione <logan@loganmarchione.com>
2023-09-07 22:25:26 -0400

    Remove provider.tf from child module

--- provider.tf
+++
@@ -1,9 +0,0 @@
-################################################################################
-### AWS
-################################################################################
-
-# Needed because CloudFront can only use ACM certs generated in us-east-1
-provider "aws" {
-  alias  = "us-east-1"
-  region = "us-east-1"
-}
