df386909429928cfdc3fb387547aba0c28f00bb6
Logan Marchione <logan@loganmarchione.com>
2023-09-09 21:03:31 -0400

    Fix formatting

--- versions.tf
+++ versions.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 1.0.2"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.15.0"
-      configuration_aliases = [ aws.us-east-1 ]
+      source                = "hashicorp/aws"
+      version               = "~> 5.15.0"
+      configuration_aliases = [aws.us-east-1]
     }
   }
 }
