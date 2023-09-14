a85c1473c877487b219b519be72c6043c15b10c8
Logan Marchione <logan@loganmarchione.com>
2023-09-14 16:31:06 -0400

    Fix output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "site_updating_iam_policy_arn" {
-  value = aws_iam_policy.site_updating[0].arn
+  description = "Value of site_updating IAM policy ARN"
+  value       = try(aws_iam_policy.site_updating[0].arn, "")
 }
